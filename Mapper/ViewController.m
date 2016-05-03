8bc1cd5b13dcd0533697fbfc17e40a28bd7c55c5
conikalpha <conikalpha@gmail.com>
2016-05-03 18:03:47 +0100

    added mapkit

--- Mapper/ViewController.m
+++ Mapper/ViewController.m
@@ -16,12 +16,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+/*
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(self.focus, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region]
+                   animated:YES];
+    
+    for (int i = 0; i < self.locations.count; i++) {
+        
+        Store *store = self.locations[i];
+        
+        
+        MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+        
+        point.coordinate = CLLocationCoordinate2DMake([store.lat doubleValue], [store.lng doubleValue]);
+        point.title = store.name;
+        point.subtitle = store.address;
+        
+        [self.mapView addAnnotation:point];
+    }
+
+*/
+
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (void)mapView:(MKMapView *)mapView
+didSelectAnnotationView:(MKAnnotationView *)view
+{
+    
+}
+
 
 @end
